be58459409f96212a1f1b2399dbe0bc5214a9ff5
DenisDemut <denis.demut@mail.ru>
2014-11-27 17:20:14 +0200

    update css - user icon bar

--- app/views/site/home.blade.php
+++ app/views/site/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 	<div class="row row-fluid col-md">
-			1111
+			
 		<div class="col-md-3">
 			<blockquote>
 				@if(!empty($user->firstname) && !empty($user->lastname))
@@ -13,14 +13,16 @@
 				@endif
 			</blockquote>
 			<div class="clearfix"></div>
-			<img class="img-rounded user-icon-images" src="/uploads/users/{{ $user->dir }}/{{ $user->icon }}" alt="{{ $user->username }}">
-			<div class="user-icon-panel text-center">
+			<div class="avatar">
+			<img class="img-rounded user-icon-images " src="/uploads/users/{{ $user->dir }}/{{ $user->icon }}" alt="{{ $user->username }}">
+			<div class="user-icon-panel text-center ">
 				
 				<div class="btn-group">
 					<a href="#" class="btn btn-sm btn-link btn-user-iconimages"><i class="fa fa-pencil-square-o"></i> Редактировать</a>	
 					<button type="button" class="btn btn-sm btn-link btn-user-iconimages" data-toggle="modal" data-target="#UploadUserIcon"><i class="fa fa-download"></i> Загрузить новую</button>
 				</div>
 				
+			</div>
 			</div>
 		</div>
 		
