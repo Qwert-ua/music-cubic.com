f63f1f4ade61799437bec7a89322a84f1d61b7d8
LeoniGor <leonigor@Leonids-MacBook-Pro.local>
2014-12-01 22:44:58 +0200

    Контролер для работы с аудиозаписями

--- app/controllers/AudioController.php
+++ app/controllers/AudioController.php
@@ -4,7 +4,32 @@ class AudioController extends Controller {
 
 	public function action_index()
 	{
-		return View::make('site.index');
+		$data = array(
+			'artists' => Artist::all()
+		);
+		
+		return View::make('site.audio_uploads', $data);
+	}
+
+	public function action_upload()
+	{
+		$data = Input::all();
+
+		$author = Translit::slug(array_get($data, 'author'));
+		$album = Translit::slug(array_get($data, 'album'));
+		$release = array_get($data, 'release');
+		
+		echo $author;
+		echo '<br />';
+		echo $release . '_' . $album;
+		echo '<br />';
+		echo $author . '_' . $release . '_' . $album . '_namefile.mp3';
+
+		/*
+
+			author -> release_album -> cover && file(author_release_album_file.mp3)
+
+		*/
 	}
 	
 	// =========================== Admin =========================== //
