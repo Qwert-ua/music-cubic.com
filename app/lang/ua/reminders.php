d081973b4c8a2892941ac5b240e00030385113c3
LeoniGor <leonigor@Leonids-MacBook-Pro.local>
2014-12-26 11:56:21 +0200

    Multi Lang

---
+++ app/lang/ua/reminders.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Паролі мають бути не менше шести символів і відповідати підтвердження.",
+
+	"user" => "Ми не можемо знайти користувача з таким e-mail.",
+
+	"token" => "Цей token скидання пароля є недійсним.",
+
+	"sent" => "Пароль надіслано!",
+
+	"reset" => "Пароль був скинутий!",
+
+);
---
+++ app/lang/ua/trans.php
@@ -0,0 +1,47 @@
+<?php
+	
+return array(
+	'form' => array(
+		'username' => 'Ім\'я користувача',
+		'first_name' => 'Ім\'я',
+		'last_name' => 'Прізвище',
+		'password' => 'Пароль',
+		'confirm_password' => 'Підтвердження пароля',
+		'birthday' => 'День народження',
+		'addrress' => 'Адреса',
+    	'member' => 'Запам\'ятати мене',
+		'author' => 'Автор',
+		'name_track' => 'Назва треку',
+		'album' => 'Альбом',
+		'year' => 'Рік випуску',
+		'cover' => 'Обкладинка',
+		'file' => 'Файл',
+		'sel_artist' => 'Виберіть артиста',
+	),
+	
+	'btn' => array(
+		'login'	=> 'Увійти',
+		'register' => 'Реєстрація',
+		'update_pers_info' => 'Оновити інформацію',
+		'edit' => 'Редагувати',
+		'upload_icon' => 'Завантажити нову',
+		'upload' => 'Завантажити',
+		'browse' => 'Вибрати'
+	),
+	
+	'menu' => array(
+		'music' => 'Музика',
+		'upload_music' => 'Завантаження музики',
+		'pers_info' => 'Персональна інформація',
+		'logout' => 'Вихід',
+		'photo' => 'Фотоальбом'
+	),
+	
+	'title' => array(
+		'auth' => 'Авторизація',
+		'edit_pers_data' => 'Редагування персональної інформації',
+		'upload_music' => 'Завантаження музики',
+		'list_music' => 'Музика',
+		'photogallery' => 'Фотоальбом',
+	)
+);
